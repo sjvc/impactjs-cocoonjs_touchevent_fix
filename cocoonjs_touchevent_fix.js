c557815b65148e73df3af5d85bf9074e60ee0289
Sergio Viudes <djpep.dj@gmail.com>
2014-01-17 13:21:58 +0100

    Make it compatible with other plugins

--- cocoonjs_touchevent_fix.js
+++ cocoonjs_touchevent_fix.js
@@ -9,15 +9,13 @@ ig.module(
 	ig.Input.inject({
 		//Override mousemove Impact event
 		mousemove: function(event){
+			this.parent(event);
+
 			if (navigator.isCocoonJS){
 				var ev = event.touches ? event.touches[0] : event;
 				this.mouse.x = ev.pageX / ig.system.scale;
 				this.mouse.y = ev.pageY / ig.system.scale;
 			}
-			else{
-				this.parent(event);
-			}
 		}
-
 	});
 });
